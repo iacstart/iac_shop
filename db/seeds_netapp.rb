0787badd495aedf195ec80e6c625287c64dcdeed
Martin Delbrück <martin.delbrueck@computacenter.com>
2020-02-25 16:09:07 +0100

    update seeds

--- db/seeds_netapp.rb
+++ db/seeds_netapp.rb
@@ -3,13 +3,13 @@ if AppSetting.find_by_id(1).nil?
   AppSetting.create(id: 1, status: "okay", version_number: "94c6a0" )
 end
 
-[*1..12].each do |d|
+%w(autsimiliqueperferendis.png culpasuntut.png eaconsequaturodio.png esserationeexcepturi.png estquidemanimi.png ipsammodiqui.png liberositexercitationem.png nequevoluptatemvoluptate.png nostrumvoluptasnumquam.png quidemomnisdolor.png veniampossimusquasi.png voluptatemrationequis.png).each do |d|
   AppDevice.create(
     manufacturer: Faker::Device.manufacturer,
     device_name: Faker::Device.model_name,
     platform: Faker::Device.platform,
     serial: Faker::Device.serial,
-    product_image: Faker::Avatar.image(size: "300x200"),
+    product_image: "images/#{d}",
     product_desc: Faker::Lorem.paragraph(sentence_count: 2)
   )
 end
